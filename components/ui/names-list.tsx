4b24d0873f404d1157349a90a6f1ced2d2b4d13e
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-09-17 13:58:24 +0200

    add categories to names-list

--- components/ui/names-list.tsx
+++ components/ui/names-list.tsx
@@ -11,11 +11,11 @@ function ListSkeleton() {
   const array = new Array(ITEMS_COUNT).fill(undefined);
 
   return (
-    <div className="text-center leading-6">
+    <div className="text-center leading-6 space-y-2">
       {array.map((_, index) => (
         <Skeleton
           key={index}
-          className="inline-block w-2/3 h-4 rounded-full bg-primary/10"
+          className="inline-block w-2/3 h-[43px] rounded-full bg-primary/10"
         />
       ))}
     </div>
@@ -27,11 +27,14 @@ export function NamesList({ data, isLoading }: Props) {
     <ListSkeleton />
   ) : (
     data.names.length > 0 && (
-      <ol className="text-center">
+      <dl className="text-center space-y-2">
         {data.names.map((concept) => (
-          <li key={concept.name}>{concept.name}</li>
+          <div key={concept.name}>
+            <dt >{concept.name}</dt>
+            <dd className="text-sm text-slate-400">{concept.category}</dd>
+          </div>
         ))}
-      </ol>
+      </dl>
     )
   );
 }
