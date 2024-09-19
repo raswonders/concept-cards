8b9f1ff87346263d5634ee6068f529174f8cd4dd
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-09-19 14:30:58 +0200

    improve skeleton for nameslist

--- components/ui/names-list.tsx
+++ components/ui/names-list.tsx
@@ -18,46 +18,28 @@ const difficultyBorderColor = {
   hard: "border-neutral-400",
 };
 
-function ListSkeleton() {
-  const ITEMS_COUNT = 3;
-  const array = new Array(ITEMS_COUNT).fill(undefined);
-
-  return (
-    <div className="text-center leading-6 space-y-2">
-      {array.map((_, index) => (
-        <Skeleton
-          key={index}
-          className="inline-block w-2/3 h-[43px] rounded-full bg-primary/10"
-        />
-      ))}
-    </div>
-  );
-}
-
 export function NamesList({ data, isLoading, difficulty }: NamesListProps) {
   const sliced = data.names.slice(
     difficultyMatrix[difficulty],
     difficultyMatrix[difficulty] + 3
   );
 
-  return (
-    <div
-      className={`border-2 p-2 rounded-lg ${difficultyBorderColor[difficulty]}`}
-    >
-      {isLoading ? (
-        <ListSkeleton />
-      ) : (
-        sliced.length > 0 && (
-          <dl className="text-center space-y-2">
-            {sliced.map((concept) => (
-              <div key={concept.name}>
-                <dt className="font-medium leading-tight">{concept.name}</dt>
-                <dd className="text-sm text-slate-400">{concept.category}</dd>
-              </div>
-            ))}
-          </dl>
-        )
-      )}
-    </div>
+  return isLoading ? (
+    <Skeleton className="w-full h-[10rem] rounded-lg bg-primary/10" />
+  ) : (
+    sliced.length > 0 && (
+      <div
+        className={`border-2 p-2 rounded-lg ${difficultyBorderColor[difficulty]}`}
+      >
+        <dl className="text-center space-y-2">
+          {sliced.map((concept) => (
+            <div key={concept.name}>
+              <dt className="font-medium leading-tight">{concept.name}</dt>
+              <dd className="text-sm text-slate-400">{concept.category}</dd>
+            </div>
+          ))}
+        </dl>
+      </div>
+    )
   );
 }
