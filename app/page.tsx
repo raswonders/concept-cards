6c46c71be498bfd235afc4d856ed2f3019b60c31
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-09-24 19:06:01 +0200

    add swipeleft action on card

--- app/page.tsx
+++ app/page.tsx
@@ -15,6 +15,7 @@ import { NamesList } from "@/components/ui/names-list";
 import { SelectCategory } from "@/components/ui/select-category";
 import { Switch } from "@/components/ui/switch";
 import { Label } from "@/components/ui/label";
+import { useSwipeable } from "react-swipeable";
 
 function createRequestBody(categoryName: string, history: History) {
   const requestBody = {
@@ -34,6 +35,13 @@ export default function Home() {
     schema: CardSchema,
     initialValue: {
       names: [],
+    },
+  });
+
+  const handlers = useSwipeable({
+    onSwipedLeft: (eventData) => {
+      submit(createRequestBody(categoryName, conceptsHistory));
+      console.log("User Swiped Left!", eventData);
     },
   });
 
@@ -71,7 +79,10 @@ export default function Home() {
   }, [object]);
 
   return (
-    <main className="min-w-0 w-full max-w-[46ch] py-10 p-6 flex flex-col gap-4 items-center">
+    <main
+      className="min-w-0 w-full max-w-[46ch] py-10 p-6 flex flex-col gap-4 items-center"
+      {...handlers}
+    >
       <Card className="w-full pt-6 ">
         <CardContent>
           {!error && (
@@ -107,7 +118,7 @@ export default function Home() {
           />
         )}
         <Button
-          className="w-full"
+          className="hidden sm:block w-full"
           disabled={isLoading}
           onClick={() => {
             submit(createRequestBody(categoryName, conceptsHistory));
